52d4440c910087293e2c5f6fc63981ef125143c7
Nimira43 <n.rabone72@aol.com>
2025-01-10 23:16:55 +0000

    Game window created.

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ class Game:
     pygame.init()
     self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption('Eezer 2')
+
+  def run(self):
+    while True:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          sys.exit()
+
+      pygame.display.update()
+
+game = Game()
+game.run()
